ac215994a68eae7f7915a0c1eaa5017772eb39e0
G M Ganesh <gmganesh173@gmail.com>
2023-02-26 00:06:05 +0530

    Changes in form for label

--- pages/careers/CreateJob.vue
+++ pages/careers/CreateJob.vue
@@ -2,145 +2,104 @@
   <div class="w-full">
     <form action="" @submit.prevent="postJob">
       <div class="flex">
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Job Title</label>
-            <FormsFormControl
-              name="jobTitle"
-              id="jobTitle"
-              type="text"
-              class="mt-3"
-              placeholder="Job Title"
-              v-model="jobData.jobTitle"
-            />
-            <!-- <label v-if="jobData.isJobTitle" class="text-red-700"
-            >Please Enter Job Title
-          </label> -->
-          </div>
+        <FormsFormField label="Job Title" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="jobTitle"
+            id="jobTitle"
+            type="text"
+            class="mt-3"
+            placeholder="Job Title"
+            v-model="jobData.jobTitle"
+            autocomplete="off"
+          />
         </FormsFormField>
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Location</label>
-            <FormsFormControl
-              name="Location"
-              id="Location"
-              type="text"
-              class="mt-3"
-              placeholder="Job Title"
-              v-model="jobData.location"
-            />
-            <!-- <label v-if="jobData.isJobLocation" class="text-red-700"
-            >Please Enter Job Title
-          </label> -->
-          </div>
+        <FormsFormField label="Job Location" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="Location"
+            id="Location"
+            type="text"
+            class="mt-3"
+            placeholder="Job Title"
+            v-model="jobData.location"
+            autocomplete="off"
+          />
         </FormsFormField>
       </div>
       <div class="flex">
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Job Description</label>
-            <FormsFormControl
-              name="jobDescription"
-              id="jobDescription"
-              type="text"
-              class="mt-3"
-              placeholder="Job Description"
-              v-model="jobData.jobDescription"
-            ></FormsFormControl>
-            <!-- <label v-if="jobData.isJobDescription" class="text-red-700"
-            >Please Enter Job Description
-          </label> -->
-          </div>
+        <FormsFormField
+          label="Job Description"
+          class="lg:col-span-6 w-5/12 ml-4"
+        >
+          <FormsFormControl
+            name="jobDescription"
+            id="jobDescription"
+            type="text"
+            class="mt-3"
+            placeholder="Job Description"
+            v-model="jobData.jobDescription"
+          ></FormsFormControl>
         </FormsFormField>
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Duties</label>
-            <FormsFormControl
-              name="duties"
-              id="duties"
-              type="text"
-              class="mt-3"
-              placeholder="Job Description"
-              v-model="jobData.duties"
-            ></FormsFormControl>
-            <!-- <label v-if="jobData.isJobDuties" class="text-red-700"
-            >Please Enter Job Duties
-          </label> -->
-          </div>
+        <FormsFormField label="Job Duties" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="duties"
+            id="duties"
+            type="text"
+            class="mt-3"
+            placeholder="Job Duties"
+            v-model="jobData.duties"
+          ></FormsFormControl>
         </FormsFormField>
       </div>
       <div class="flex">
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Requirements</label>
-            <FormsFormControl
-              name="requirements"
-              id="requirements"
-              type="text"
-              class="mt-3"
-              placeholder="Job Description "
-              v-model="jobData.requirements"
-            ></FormsFormControl>
-            <!-- <label v-if="jobData.isRequirements" class="text-red-700"
-            >Please Enter Job Requirements
-          </label> -->
-          </div>
+        <FormsFormField
+          label="Job Requirements"
+          class="lg:col-span-6 w-5/12 ml-4"
+        >
+          <FormsFormControl
+            name="requirements"
+            id="requirements"
+            type="text"
+            class="mt-3"
+            placeholder="Job Requirements "
+            v-model="jobData.requirements"
+          ></FormsFormControl>
         </FormsFormField>
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Nice To Have</label>
-            <FormsFormControl
-              name="tohave"
-              id="tohave"
-              type="text"
-              class="mt-3"
-              placeholder="Job Description"
-              v-model="jobData.tohave"
-            ></FormsFormControl>
-            <!-- <label v-if="jobData.isToHave" class="text-red-700"
-            >Please Enter Nice To Have
-          </label> -->
-          </div>
+        <FormsFormField label="Nice To Have" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="tohave"
+            id="tohave"
+            type="text"
+            class="mt-3"
+            placeholder="Nice To Have"
+            v-model="jobData.tohave"
+          ></FormsFormControl>
         </FormsFormField>
       </div>
       <div class="flex">
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Benefits</label>
-            <FormsFormControl
-              name="benefits"
-              id="benefits"
-              type="text"
-              class="mt-3"
-              placeholder="Job Description"
-              v-model="jobData.benefits"
-            ></FormsFormControl>
-            <!-- <label v-if="jobData.isBenefits" class="text-red-700"
-            >Please Enter Benefits
-          </label> -->
-          </div>
+        <FormsFormField label="Benefits" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="benefits"
+            id="benefits"
+            type="text"
+            class="mt-3"
+            placeholder="Benefits"
+            v-model="jobData.benefits"
+          ></FormsFormControl>
         </FormsFormField>
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Salary Range</label>
-            <FormsFormControl
-              name="salaryRange"
-              id="salaryRange"
-              type="text"
-              class="mt-3"
-              placeholder="Job Title"
-              v-model="jobData.salaryRange"
-            />
-            <!-- <label v-if="jobData.isSalaryRange" class="text-red-700"
-            >Please Enter Salary Range
-          </label> -->
-          </div>
+        <FormsFormField label="Salary Range" class="lg:col-span-6 w-5/12 ml-4">
+          <FormsFormControl
+            name="salaryRange"
+            id="salaryRange"
+            type="text"
+            class="mt-3"
+            placeholder="Salary Range"
+            v-model="jobData.salaryRange"
+          />
         </FormsFormField>
       </div>
       <div class="flex">
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Department</label>
+        <FormsFormField label="Department" class="lg:col-span-6 w-5/12 ml-4">
+          <div class="ltr:text-left rtl:text-right grid">
             <select class="mt-3 rounded-md h-12" v-model="jobData.department">
               <option>B.Sc</option>
               <option>B.C.A</option>
@@ -148,23 +107,15 @@
               <option>B.Tech</option>
               <option>M.Tech</option>
             </select>
-            <!-- <label v-if="jobData.isDepartment" class="text-red-700"
-            >Please Select Department
-          </label> -->
           </div>
         </FormsFormField>
-
-        <FormsFormField class="lg:col-span-6 w-5/12">
-          <div class="ltr:text-left rtl:text-right grid ml-4">
-            <label for="name" class="font-semibold">Job Type</label>
+        <FormsFormField label="Job Type" class="lg:col-span-6 w-5/12 ml-4">
+          <div class="ltr:text-left rtl:text-right grid">
             <select class="mt-3 rounded-md h-12" v-model="jobData.jobType">
               <option>Full Time</option>
               <option>Part Time</option>
               <option>Freelance</option>
             </select>
-            <!-- <label v-if="jobData.isJobType" class="text-red-700"
-            >Please Select Job Type
-          </label> -->
           </div>
         </FormsFormField>
       </div>

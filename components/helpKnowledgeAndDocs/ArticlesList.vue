c231e3856fc3bf25fafdcb49b4cfabb50af3a919
neeteshking21 <neeteshking21@gmail.com>
2023-03-16 16:01:51 +0530

    [FEAT]:: User Article Lists UI & UX

---
+++ components/helpKnowledgeAndDocs/ArticlesList.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  articles: {
+    type: Array,
+  },
+});
+
+const _router = useRouter();
+</script>
+<template>
+  <div
+    class="border-2 border-gray-200 dark:border-gray-600 rounded dark:bg-slate-900/70 bg-white px-6 flex  flex-wrap gap-16 py-2 bg-"
+  >
+    <ul
+      class="list-disc list-outside ml-4 mt-3"
+      v-for="(article_info, category) in articles"
+    >
+      <li>
+        <span class="text-xl text-ellipsis ">{{ category }}</span>
+        <ul class="list-decimal list-outside ml-4 mt-3 flex flex-col gap-3">
+          <li v-for="(article, article_id) in article_info" :key="article_id">
+            <span
+              class="text-blue-600 hover:cursor-pointer hover:underline ml-2 text-ellipsis tracking-wider"
+              @click="
+                _router.push(`/helpKnowledgeAndDocs/article/${article_id}`)
+              "
+            >
+              {{ article.title }}
+            </span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ configs/sampleArticles.js
@@ -0,0 +1,117 @@
+export const sampleArticlesByCategory = {
+    Tech: {
+      1: {
+        category: "Tech",
+        title: "My First Tech",
+        article: "Hey s",
+      },
+      2: {
+        category: "Tech",
+        title: "My Second Tech",
+        article: "Hey There",
+      },
+    },
+    Education: {
+      3: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      4: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+    },
+    Educatioqn: {
+      5: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      6: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+    },
+    Educatiodn: {
+      7: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      8: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+    },
+    Educatiaon: {
+     9: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      10: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      }
+    }
+    
+  }
+
+  export const sampleArticles = {
+      1: {
+        category: "Tech",
+        title: "My First Tech",
+        article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. <b>8888888888888888</b>Rem facilis eum ratione iusto! Similique praesentium, architecto natus dignissimos neque rem reprehenderit alias optio, maiores suscipit impedit fugiat. Praesentium cumque repudiandae aliquid quisquam veritatis maiores labore animi nemo assumenda libero architecto aut a est quae temporibus, eveniet pariatur molestias, dolor iusto! Quae consectetur veniam ut, ad debitis, voluptate dolores quaerat consequuntur tempora, at dicta repudiandae! Iure reiciendis, consectetur vero alias dolore facilis et qui sapiente voluptatem nostrum voluptate, deleniti explicabo, magni ex iste nulla eveniet optio non nihil facere! Omnis reiciendis itaque quisquam, in sunt nulla saepe modi sequi adipisci quaerat error quae hic iure! Recusandae sed ullam nihil fugiat, quis, iure deleniti consequatur ab nulla magni voluptatem et molestias, quo quia quod unde eius error dolorum voluptatibus. Quo voluptates quia explicabo laudantium voluptate libero, quis commodi quas blanditiis aperiam nam eaque ipsa corporis repellat, esse tenetur minus inventore reprehenderit? Eius, minima error, ratione inventore numquam modi illum placeat quam, blanditiis iure facere nobis architecto officia quibusdam! Doloribus, tenetur sapiente non eius iusto incidunt esse dolorem earum exercitationem expedita quam accusantium quos qui eum blanditiis dolores repudiandae minima facere dolore reiciendis hic officiis deleniti, rerum ducimus? Voluptatem reprehenderit placeat incidunt ipsa earum, veniam veritatis magni sunt repellendus debitis consectetur, maiores velit obcaecati minus unde laborum accusantium aspernatur in vero nesciunt impedit. Consequatur, quod temporibus aperiam, dignissimos cumque sit eligendi aliquid recusandae numquam cupiditate labore dolor. Itaque possimus autem ipsum! Fuga ipsum doloribus ea magni? In ad enim animi harum nostrum assumenda qui aliquid eos facere ullam, quia ab sapiente saepe quae, alias inventore libero repudiandae impedit quis dolorum iste cumque temporibus, ratione error! Ipsam nihil dolorem pariatur iure animi laudantium tempore harum perferendis, rem velit officiis non fugiat illum unde repudiandae? Aspernatur quod asperiores vel voluptatum quisquam dolore repellat numquam, iure accusamus aliquid quos error natus. Voluptatum animi minus dolorum deserunt, sint fuga facere cum id quaerat nulla dignissimos molestias voluptatem aperiam. Esse architecto sit, temporibus magnam doloremque ipsa, deleniti corrupti error, debitis repellat quibusdam nobis cumque ullam enim. Iusto libero tenetur cupiditate architecto distinctio laudantium natus dolore, amet nam impedit modi commodi corporis quo adipisci velit debitis autem assumenda dolores doloremque soluta minus laborum ad? Non pariatur magnam hic libero, quae temporibus voluptatibus exercitationem doloribus nemo velit ad quis officia commodi voluptatum saepe unde eius cupiditate obcaecati accusamus at laudantium aspernatur eveniet? Natus impedit aliquam aperiam illum quidem commodi temporibus id, non dicta cum ipsam sed architecto nesciunt sapiente nostrum laudantium repudiandae, quos numquam saepe, dignissimos eum libero voluptatem eos alias! Vel sed iure nesciunt sunt, doloremque reprehenderit expedita, eum iste magnam quibusdam quasi quia doloribus eius eaque illum labore voluptatibus ut, nam autem. Natus molestias iste beatae, libero id eveniet rem nisi vero? Ut non consequuntur harum deserunt unde eos laborum eligendi, quasi magnam vitae quas tempora, doloremque assumenda est incidunt, nihil saepe nemo ipsam maxime perferendis dolores a nesciunt. Doloribus maiores expedita, totam omnis quos ratione animi aut soluta unde dicta eaque a odit, possimus mollitia fugiat amet dolor. Quod inventore mollitia rerum vitae, accusantium praesentium minus a itaque alias commodi eius magnam voluptatum placeat totam voluptates numquam debitis porro unde? Dolorem quibusdam velit a, pariatur impedit sunt nulla earum et beatae dignissimos odio adipisci dolore fugit aspernatur minima officia officiis necessitatibus delectus doloremque aliquid? Reprehenderit saepe quos, id accusantium hic dolorum. In animi esse eaque doloribus vero ipsam exercitationem quibusdam sit ratione, saepe quaerat rerum? Obcaecati excepturi alias expedita dolores, consequatur vitae illo rerum fugiat iusto perferendis? Quos ipsum ullam sequi numquam velit fugiat error accusantium, nisi exercitationem placeat tempore facere id voluptas delectus qui dicta reprehenderit suscipit odio doloribus iure rem natus! Nostrum aut dolores porro, eligendi illo magni necessitatibus odio magnam sint ratione, iusto nemo itaque fugiat pariatur sit facilis tempora quia, animi reiciendis ad! Et, laboriosam blanditiis porro at illo enim unde repellat rem accusamus aperiam error veniam facere praesentium. Ex cum ipsam, vel id nemo laboriosam quisquam veritatis obcaecati eius modi facere fuga placeat voluptate nihil sint dolores porro optio? Placeat, dolores velit. Debitis sit minima dignissimos, omnis, natus veritatis iste voluptatum voluptas doloremque explicabo iure suscipit nostrum quidem repellendus dolores nam quia et quasi quam aut eveniet cupiditate enim? Nam magni consequatur, modi, nesciunt similique aperiam commodi, qui tenetur quos animi quod eos reprehenderit. Quisquam ut quasi, illum esse magnam atque minima porro, harum cupiditate blanditiis facere! Aut ullam deleniti in ut explicabo odio esse molestiae soluta rerum perspiciatis quia magnam accusamus placeat, minima totam officia excepturi unde architecto consectetur non nisi perferendis illo assumenda eos. Ducimus, velit magnam? Impedit modi officia neque fuga, adipisci reiciendis corporis ut voluptatem ullam? Quidem officia eius, est voluptas nulla accusamus possimus repellat nisi in molestias debitis. Amet optio exercitationem quod obcaecati nam. Tempora voluptatem corrupti autem, sapiente repudiandae illum itaque? Quibusdam at molestiae nam, accusantium ex placeat ab natus inventore eos laboriosam pariatur dolorem officiis recusandae ut. Incidunt iste voluptatem, delectus error eveniet molestiae fugiat cupiditate perferendis a saepe labore praesentium veritatis, quis, provident reiciendis libero itaque! Sint iusto voluptas doloremque dolorem delectus hic explicabo quo, possimus exercitationem quisquam ab. Libero, facilis, debitis nesciunt rerum voluptatum, omnis dolorum temporibus officiis maxime enim quia excepturi. Totam aliquid laboriosam consequatur? Hic fuga adipisci quae nobis! Ducimus sequi voluptatem, accusantium necessitatibus pariatur explicabo ullam, iste quidem vitae quas, facere voluptates minus dolor suscipit ipsum iure reprehenderit nemo harum! Impedit possimus autem quam? Officia repellat labore minima voluptas, voluptatum dolor amet fuga ratione dolorem, nisi, laboriosam sed delectus obcaecati quod. Et, eveniet. Dignissimos eligendi quae ratione, non corporis expedita animi blanditiis quam, eaque nam aliquid a quibusdam aspernatur. Id eveniet laudantium nesciunt incidunt blanditiis sit odit eaque earum labore fugit amet deserunt error soluta, cum dignissimos similique totam! Earum omnis, dolores dolor debitis beatae ducimus hic eligendi autem. Quo, praesentium? Maiores, et. Repudiandae magnam saepe, neque hic deleniti quibusdam eius consequuntur qui, consequatur, dignissimos fugiat pariatur explicabo. Rerum nam porro deserunt doloremque, perferendis assumenda sunt unde. Alias odit repellat rerum sint, ad assumenda. Eaque aperiam dolore architecto beatae dolorem!",
+      },
+      2: {
+        category: "Tech",
+        title: "My Second Tech",
+        article: "Hey There",
+      },
+      3: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      4: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+      5: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      6: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+      7: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      8: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      },
+     9: {
+        category: "Education",
+        title: "My First Education",
+        article: "Hey Edu1",
+      },
+      10: {
+        category: "Education",
+        title: "My Second Education",
+        article: "Hey Edu2",
+      }
+    
+  }

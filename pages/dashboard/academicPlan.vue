58d9f3b2f229f0552f1f58e3ee53fc7c80a701a0
mohit soni <imohitsoni28@gmail.com>
2023-03-28 15:36:35 +0530

    minor bugs

--- pages/dashboard/academicPlan.vue
+++ pages/dashboard/academicPlan.vue
@@ -16,27 +16,7 @@
               {{ key }}
             </th>
           </tr>
-          <!-- <tr
-            class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
-          >
-            <th class="p-3 text-left">Name</th>
-            <th class="p-3 text-left">Email</th>
-            <th class="p-3 text-left" width="110px">Actions</th>
-          </tr>
-          <tr
-            class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
-          >
-            <th class="p-3 text-left">Name</th>
-            <th class="p-3 text-left">Email</th>
-            <th class="p-3 text-left" width="110px">Actions</th>
-          </tr>
-          <tr
-            class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0"
-          >
-            <th class="p-3 text-left">Name</th>
-            <th class="p-3 text-left">Email</th>
-            <th class="p-3 text-left" width="110px">Actions</th>
-          </tr> -->
+
         </thead>
         <tbody class="flex-1 lg:flex-none">
           <tr
@@ -60,7 +40,6 @@
                   </a>
                 </div>
                 <div class="flex items-center justify-center">
-                  <!-- {{ colm.status }} -->
                   <select
                     v-model="colm.status"
                     id="underline_select"
@@ -75,43 +54,6 @@
               </div>
             </td>
           </tr>
-          <!-- <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-          <td class="border-grey-light border hover:bg-gray-100 p-3">
-            Michael Jackson
-          </td>
-          <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">
-            m_jackson@mail.com
-          </td>
-          <td
-            class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer"
-          >
-            Delete
-          </td>
-        </tr>
-        <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-          <td class="border-grey-light border hover:bg-gray-100 p-3">Julia</td>
-          <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">
-            julia@mail.com
-          </td>
-          <td
-            class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer"
-          >
-            Delete
-          </td>
-        </tr>
-        <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
-          <td class="border-grey-light border hover:bg-gray-100 p-3">
-            Martin Madrazo
-          </td>
-          <td class="border-grey-light border hover:bg-gray-100 p-3 truncate">
-            martin.madrazo@mail.com
-          </td>
-          <td
-            class="border-grey-light border hover:bg-gray-100 p-3 text-red-400 hover:text-red-600 hover:font-medium cursor-pointer"
-          >
-            Delete
-          </td>
-        </tr> -->
         </tbody>
       </table>
     </div>

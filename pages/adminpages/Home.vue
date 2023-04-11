ea3277e81a0d5ed03b082fa60c1b02e83689b410
Ranjeetkumar641 <ranjeetk641@gmail.com>
2023-04-11 23:32:36 +0530

    adminpage

---
+++ pages/adminpages/Home.vue
@@ -0,0 +1,217 @@
+<script setup>
+import { ref, computed, watch, onMounted } from "vue";
+import { useMainStore } from "@/stores/main.js";
+import { useSnackBarStore } from "@/stores/snackBar.js";
+import { useLayoutStore } from "@/stores/layout.js";
+import {
+  mdiAccountMultiple,
+  mdiCartOutline,
+  mdiChartTimelineVariant,
+  mdiMonitorCellphone,
+  mdiReload,
+  mdiBell,
+  mdiPlusCircle,
+  mdiBroadcast,
+  mdiBriefcaseVariant,
+  mdiCalendarToday,
+  mdiCameraSwitch,
+  mdiChartPie,
+  mdiCreditCardOutline,
+} from "@mdi/js";
+import * as chartConfig from "@/components/Charts/chart.config.js";
+import LineChart from "@/components/Charts/LineChart.vue";
+import SectionMain from "@/components/Sections/SectionMain.vue";
+import SectionTitleLineWithButton from "@/components/Sections/SectionTitleLineWithButton.vue";
+import CardBoxWidget from "@/components/Cards/CardBoxWidget.vue";
+import CardBox from "@/components/Cards/CardBox.vue";
+import TableSampleClients from "@/components/Tables/TableSampleClients.vue";
+import NotificationBar from "@/components/NotificationBars/NotificationBar.vue";
+import CardBoxClient from "@/components/Cards/CardBoxClient.vue";
+import CardBoxTransaction from "@/components/Cards/CardBoxTransaction.vue";
+import BaseButtons from "@/components/Buttons/BaseButtons.vue";
+import BaseButton from "@/components/Buttons/BaseButton.vue";
+
+import PremUserCard from "@/components/Cards/UserCard.vue";
+import PremCardBoxProduct from "@/components/Cards/CardBoxProduct.vue";
+import PremCardBoxAmountItem from "@/components/Cards/CardBoxAmountItem.vue";
+
+const mainStore = useMainStore();
+
+const snackBarStore = useSnackBarStore();
+
+snackBarStore.pushMessage("Welcome back. This is demo", "contrast");
+
+const userSwitchVal = ref(true);
+
+watch(userSwitchVal, (value) => {
+  if (value) {
+    snackBarStore.pushMessage("Success! Now active", "success");
+  } else {
+    snackBarStore.pushMessage("Done! Now inactive", "danger");
+  }
+});
+
+const layoutStore = useLayoutStore();
+
+const isLg = computed(() => layoutStore.isLg);
+
+const isMd = computed(() => layoutStore.isMd);
+
+watch([isLg, isMd], () => {
+  fillChartData();
+});
+
+const chartData = ref(null);
+
+const fillChartData = () => {
+  let points = 4;
+
+  if (isLg.value) {
+    points = 9;
+  } else if (isMd.value) {
+    points = 6;
+  }
+
+  chartData.value = chartConfig.sampleChartData(points);
+};
+
+onMounted(() => {
+  fillChartData();
+});
+
+const clientBarItems = computed(() => mainStore.clients.slice(0, 3));
+
+const transactionBarItems = computed(() => mainStore.history);
+
+const productBarItems = computed(() => mainStore.products);
+</script>
+
+<template>
+  <div>
+    <NuxtLayout name="zen">
+      <SectionMain>
+        <SectionTitleLineWithButton
+          :icon="mdiChartTimelineVariant"
+          title="JavaScript"
+          main
+        >
+          <BaseButton
+            href="https://justboil.me/tailwind-admin-templates/vue-dashboard/"
+            label="Buy dashboard"
+            :icon="mdiCreditCardOutline"
+            color="contrast"
+            rounded-full
+            small
+          />
+        </SectionTitleLineWithButton>
+        <div class="grid grid-cols-12 gap-6 mb-6 -mx-6 md:mx-0">
+          <div class="col-span-12 sm:col-span-6 xl:col-span-3">
+            <CardBoxWidget
+              trend="12%"
+              trend-type="up"
+              color="text-emerald-500"
+              :icon="mdiAccountMultiple"
+              :number="2015"
+              label="javascript version :ES6"
+            />
+          </div>
+          
+          <div class="col-span-12 sm:col-span-6 xl:col-span-3">
+            <CardBoxWidget
+              trend="Overflow"
+              trend-type="alert"
+              color="text-red-500"
+              :icon="mdiChartTimelineVariant"
+              :number="100"
+              suffix="%"
+              label="Working of javascript"
+            />
+          </div>
+          <div class="col-span-12 sm:col-span-6 xl:col-span-3">
+            <CardBoxWidget
+              trend="Last 24 hours"
+              trend-type="info"
+              color="text-yellow-500"
+              :icon="mdiBell"
+              :number="223"
+              label="score"
+            />
+          </div>
+        </div>
+
+        <div class="grid grid-cols-1 xl:grid-cols-2 gap-6 mb-6">
+          <PremUserCard />
+          <div class="flex flex-col">
+            <CardBoxClient
+              v-for="client in clientBarItems"
+              :key="client.id"
+              :name="client.name"
+              :login="client.login"
+              :date="client.created"
+              :progress="client.progress"
+            />
+          </div>
+        </div>
+
+        <SectionTitleLineWithButton
+          :icon="mdiChartTimelineVariant"
+          title="Finance"
+        />
+
+        <div class="grid grid-cols-1 xl:grid-cols-4 xl:gap-6 mb-6">
+          <div class="xl:flex xl:flex-col xl:col-span-3 mb-6 xl:mb-0">
+            <CardBoxTransaction
+              v-for="(transaction, index) in transactionBarItems"
+              :key="index"
+              :amount="transaction.amount"
+              :date="transaction.date"
+              :business="transaction.business"
+              :type="transaction.type"
+              :name="transaction.name"
+              :account="transaction.account"
+            />
+          </div>
+          
+        </div>
+
+        <SectionTitleLineWithButton :icon="mdiChartPie" title="Trends overview">
+          <BaseButton
+            :icon="mdiReload"
+            color="whiteDark"
+            @click="fillChartData"
+          />
+        </SectionTitleLineWithButton>
+
+        <CardBox class="mb-6">
+          <div class="grid grid-cols-1 gap-6 md:grid-cols-4">
+            <div v-if="chartData" class="md:col-span-3">
+              <LineChart :data="chartData" />
+            </div>
+            
+          </div>
+        </CardBox>
+
+        <SectionTitleLineWithButton :icon="mdiChartPie" title="Products" />
+
+        <PremCardBoxProduct
+          v-for="product in productBarItems"
+          :key="product.id"
+          :product="product"
+        />
+
+        <SectionTitleLineWithButton
+          :icon="mdiAccountMultiple"
+          title="Clients"
+        />
+
+        <NotificationBar color="info" :icon="mdiMonitorCellphone">
+          <b>Responsive table.</b> Collapses on mobile
+        </NotificationBar>
+
+        <CardBox has-table>
+          <TableSampleClients />
+        </CardBox>
+      </SectionMain>
+    </NuxtLayout>
+  </div>
+</template>

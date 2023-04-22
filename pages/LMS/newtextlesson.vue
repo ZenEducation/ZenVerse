67b869cadfec5e23b5618cb8b1d15126163f69ba
pranavbhaskar02 <pbpranavbhaskar@gmail.com>
2023-04-23 01:11:09 +0530

    Added ref for Title

--- pages/LMS/newtextlesson.vue
+++ pages/LMS/newtextlesson.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { ref, } from "vue";
     import SectionMain from "@/components/Sections/SectionMain.vue";
     import CardBox from "@/components/Cards/CardBox.vue";
     import CardBoxComponentTitle from "@/components/Cards/CardBoxComponentTitle.vue";
@@ -11,10 +12,32 @@
     import SectionTitleLineWithButton from "~~/components/Sections/SectionTitleLineWithButton.vue";
     import { QuillEditor } from "@vueup/vue-quill";
     import '@vueup/vue-quill/dist/vue-quill.snow.css';
+    import { mdiTrashCan } from "@mdi/js";
 
-    const draftOption= {
+    const draftOption = {
         one: "Draft"
     }
+
+    const toolbarOptions = [
+        [{'direction': 'rtl'}],
+        [{'size': ['small', false, 'large', 'huge']}],
+        [{'header':[1,2,3,4,5,6,false]}],
+        [{'font': []}],
+        [{'header':1},{'header':2}],
+        [{'color':[]},{'background':[]}],
+        ['bold','italic','underline','strike'],
+        [{'script': 'sub'}, {'script': 'super'}],
+        [{'list':'ordered'},{'list':'bullet'}],
+        [{'align':[]}],
+        [{'indent':'-1'},{'indent':'+1'}],
+        ['blockquote'],
+        ['image','video','link'],
+        ['code-block'],
+        ['clean']
+    ]
+
+    const titleText = ref('New Text Lesson');
+    const newText = "New Text";
 
 </script>
 
@@ -25,10 +48,26 @@
                 <SectionTitleLineWithButton
                     main
                 >
-                    <FormCheckRadioGroup
+                    <BaseButtons>
+                        <FormCheckRadioGroup
                         class="font-bold"
                         :options="draftOption"
-                    />
+                        style="transform: translateY(15%);"
+                        />
+                        <svg style="transform: translateY(-30%); margin-right: 25px; " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 7v2h2V7h-2m3 10v-2h-1v-4h-3v2h1v2h-1v2h4m8-5c0 5.5-4.5 10-10 10S2 17.5 2 12S6.5 2 12 2s10 4.5 10 10m-2 0c0-4.42-3.58-8-8-8s-8 3.58-8 8s3.58 8 8 8s8-3.58 8-8Z"/></svg>
+                        <BaseButton 
+                            type="submit" 
+                            color="danger" 
+                            :icon="mdiTrashCan" 
+                            label="DISCARD CHANGES"
+                            rounded-full 
+                            outline/>
+                        <BaseButton 
+                            type="submit" 
+                            color="contrast" 
+                            label="SAVE"
+                            rounded-full/> 
+                    </BaseButtons>
                 </SectionTitleLineWithButton>
                 <div class="gird gird-cols-1 gap-6 mb-6 xl:grid-cols-4">
                     <CardBox 
@@ -36,25 +75,22 @@
                         if-from
                         @submit.prevent="submit"    
                     >
-                        <CardBoxComponentTitle
-                            title="New Lesson"
+                        <!-- <CardBoxComponentTitle
+                            title={{ titleText }}
                             class="mb-12"
-                        />
+                        /> -->
+                        <h1 class="text-3xl text-dark-700">{{ titleText }}</h1>
                         <BaseDivider/>
                         <PremFormField label="Title">
                             <PremFormControl
+                                v-model="titleText"
                                 placeholder="First Lesson"
                             />
                         </PremFormField>
                         <PremFormField label="Content">
-                            <QuillEditor style="height: 300px;" theme="snow" toolbar="full"/>
+                            <QuillEditor style="height: 300px;" theme="snow" :toolbar="toolbarOptions"/>
                         </PremFormField>
                         <BaseDivider/>
-                        <BaseButtons>
-                            <BaseButton type="submit" color="contrast" label="SAVE"/>
-                            <BaseButton type="submit" color="danger" :icon="mdiTrashCan" label="DISCARD CHANGES" outline/> 
-                        </BaseButtons>
-
                     </CardBox>
                 </div>
             </SectionMain>

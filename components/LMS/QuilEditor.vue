cf52e597f334e185c36c763e43a5be2d5611ea12
Anisha12311 <2000anishayadav@gmail.com>
2023-04-28 18:08:20 +0530

    add images

--- components/LMS/QuilEditor.vue
+++ components/LMS/QuilEditor.vue
@@ -5,6 +5,23 @@ import BlotFormatter from 'quill-blot-formatter'
 import ImageUploader from 'quill-image-uploader';
 import axios from 'axios';
 import QuillBetterTable from 'quill-better-table'
+const toolbarOptions = [
+        [{'direction': 'rtl'}],
+        [{'size': ['small', false, 'large', 'huge']}],
+        [{'header':[1,2,3,4,5,6,false]}],
+        [{'font': []}],
+        [{'header':1},{'header':2}],
+        [{'color':[]},{'background':[]}],
+        ['bold','italic','underline','strike'],
+        [{'script': 'sub'}, {'script': 'super'}],
+        [{'list':'ordered'},{'list':'bullet'}],
+        [{'align':[]}],
+        [{'indent':'-1'},{'indent':'+1'}],
+        ['blockquote'],
+        ['image','video','link'],
+        ['code-block'],
+        ['clean']
+    ]
 const modules = { 
         module: BlotFormatter, 
         module: ImageUploader,
@@ -26,5 +43,5 @@ const modules = {
         }}
 </script>
 <template>
-    <QuillEditor  toolbar="full" :modules="modules" theme="snow" />
+      <QuillEditor style="min-height: 30vh;" theme="snow" :toolbar="toolbarOptions"/>
 </template>

bf090360f33cbc9aeeff9c1997e12f2a07cc1e08
Bhavya Tiwari <109498434+bhavyatiwarigithub@users.noreply.github.com>
2023-05-12 22:38:32 +0530

    Create CandidateSkillSingle.vue

---
+++ components/RecuitmentPortal/CandidateSkillSingle.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="mt-4">
+    <div class="flex justify-between mb-2">
+        <span class="text-slate-400">{{props.skillName}}</span>
+        <span class="text-slate-400">{{props.num}}%</span>
+    </div>
+    <div class="w-full bg-gray-100 dark:bg-gray-800 rounded-full h-[6px]">
+        <div class="bg-indigo-600 h-[6px] rounded-full" :style="width"></div>
+    </div>
+</div>
+</template>
+
+<script setup>
+import {
+    defineProps
+} from 'vue'
+
+const props = defineProps({
+    skillName: String,
+    num: {
+        type: String,
+        default:"0"
+        
+    }
+})
+const width = "width: "+props.num+"%";
+
+</script>

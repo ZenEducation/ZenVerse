cd9eb08854f6b171461fee68d70ab3afecf5203b
mohit soni <imohitsoni28@gmail.com>
2023-05-23 17:51:30 +0530

    update

--- pages/LMS/PdfView.vue
+++ pages/LMS/PdfView.vue
@@ -59,7 +59,7 @@ onMounted(() => {
     <NuxtLayout name="pdf">
       <LMSPdfViewSectionCarrier>
         <h1>hello</h1>
-        <iframe id="pdf-iframe"  class="pl-4 w-full h-full"> this is div area  </iframe>
+        <iframe id="pdf-iframe"  class="pl-4 w-full h-full">   </iframe>
       </LMSPdfViewSectionCarrier>
     </NuxtLayout>
   </div>

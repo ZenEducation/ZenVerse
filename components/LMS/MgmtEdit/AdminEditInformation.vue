e0295eefd744d8188d44a79f6aa13d1ce99f7174
mohit soni <imohitsoni28@gmail.com>
2023-05-28 00:40:48 +0530

    admin instuctor added

--- components/LMS/MgmtEdit/AdminEditInformation.vue
+++ components/LMS/MgmtEdit/AdminEditInformation.vue
@@ -4,39 +4,39 @@
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <FormField label="Name">
         <FormControl
-          :model-value="learner.profile.name"
-          placeholder="Enter learner Name"
+          :model-value="Admin.profile.name"
+          placeholder="Enter Admin Name"
         />
       </FormField>
 
       <FormField label="E-mail">
         <FormControl
-          :model-value="learner.profile.email"
-          placeholder="Enter learner email"
+          :model-value="Admin.profile.email"
+          placeholder="Enter Admin email"
         />
       </FormField>
 
       <FormField label="Mobile">
         <FormControl
-          :model-value="learner.profile.mobile"
-          placeholder="Enter learner Mobile"
+          :model-value="Admin.profile.mobile"
+          placeholder="Enter Admin Mobile"
         />
       </FormField>
       <FormField label="Role">
         <FormControl
-          :model-value="learner.profile.role"
+          :model-value="Admin.profile.role"
           :options="options.profileRoles"
         />
       </FormField>
       <FormField label="State">
         <FormControl
-          :model-value="learner.profile.state"
+          :model-value="Admin.profile.state"
           :options="options.state"
         />
       </FormField>
       <FormField label="Language">
         <FormControl
-          :model-value="learner.profile.language"
+          :model-value="Admin.profile.language"
           :options="options.language"
         />
       </FormField>
@@ -49,13 +49,13 @@
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <FormField label="User Segment">
         <FormControl
-          :model-value="learner.AdditionalDetails.UserSegment"
+          :model-value="Admin.AdditionalDetails.UserSegment"
           :options="options.UserSegment"
         />
       </FormField>
       <FormField label="Lead Status">
         <FormControl
-          :model-value="learner.AdditionalDetails.LeadStatus"
+          :model-value="Admin.AdditionalDetails.LeadStatus"
           :options="options.LeadStatus"
         />
       </FormField>
@@ -67,27 +67,6 @@
       <div class="flex flex-row-reverse">
         <BaseButton color="info" label="Save" />
       </div>
-    </div>
-
-    <h2 class="text-2xl mt-8 font-semibold">Unblock Learner on iOS</h2>
-    <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p>
-        Learner's accounts are blocked if they try to take more than 3 screenshots in the iOS app.
-On every screenshot taken, they are presented with the warning of account blockage.
-You can click on the below button to unblock the account
-      </p>
-      <br>
-      <h2>Total screenshots taken : <span class="text-blue-400">0</span></h2>
-      <div class="flex mt-4">
-        <BaseButton color="info" label="Unblock Learner" />
-      </div>
-    </div>
-
-
-
-    <h2 class="text-2xl mt-8 font-semibold">Downloaded Courses</h2>
-    <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      No Downloaded Courses
     </div>
 
     <h2 class="text-2xl mt-8 font-semibold">Change Password</h2>
@@ -111,20 +90,22 @@ You can click on the below button to unblock the account
 
     <h2 class="text-2xl mt-8 font-semibold">Referral Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p v-for="(value , key) in learner.ReferralDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
+      <p v-for="(value , key) in Admin.ReferralDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
 
     </div>
 
     <h2 class="text-2xl mt-8 font-semibold">UTM Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p v-for="(value , key) in learner.UTMDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
+      <p v-for="(value , key) in Admin.UTMDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
 
     </div>
+
     <h2 class="text-2xl mt-8 font-semibold">Login Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <p class="py-2">Number of Logins : <span class="font-bold">60</span></p>
       <p class="py-2">Login Device Reset Count : <span class="font-bold">4</span></p>
       <p class="py-2">last login date : <span class="font-bold">{{new Date("Mar 03 , 2023")}}</span></p>
+
       <form class="relative" @submit.prevent="submit">
         <label for="msg-search" class="sr-only">Search</label>
         <input
@@ -183,6 +164,7 @@ You can click on the below button to unblock the account
 
     </div>
 
+
   </div>
 </template>
 
@@ -195,7 +177,7 @@ import BaseButtons from "@/components/Buttons/BaseButtons.vue";
 import BaseIcon from "@/components/Display/BaseIcon.vue";
 
 const props = defineProps({
-  learner: {
+  Admin: {
     type: Object,
     required: true,
   },
@@ -204,10 +186,9 @@ const props = defineProps({
     required: true,
   },
 });
-
 const searchQuery = ref("");
 
-const items = ref(props?.learner?.LoginDetails?.['last logins'])
+const items = ref(props?.Admin?.LoginDetails?.['last logins'])
 
 const filteredItems = computed(()=>{
   let filtered = items.value;
@@ -222,7 +203,6 @@ const filteredItems = computed(()=>{
   }
   return filtered
 })
-
 </script>
 
 <style lang="scss" scoped></style>
--- components/LMS/MgmtEdit/InstuctorEditInformation.vue
+++ components/LMS/MgmtEdit/InstuctorEditInformation.vue
@@ -4,39 +4,56 @@
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <FormField label="Name">
         <FormControl
-          :model-value="learner.profile.name"
-          placeholder="Enter learner Name"
+          :model-value="Instructor.profile.name"
+          placeholder="Enter Instructor Name"
         />
       </FormField>
 
       <FormField label="E-mail">
         <FormControl
-          :model-value="learner.profile.email"
-          placeholder="Enter learner email"
+          :model-value="Instructor.profile.email"
+          placeholder="Enter Instructor email"
         />
       </FormField>
 
       <FormField label="Mobile">
         <FormControl
-          :model-value="learner.profile.mobile"
-          placeholder="Enter learner Mobile"
+          :model-value="Instructor.profile.mobile"
+          placeholder="Enter Instructor Mobile"
         />
       </FormField>
       <FormField label="Role">
         <FormControl
-          :model-value="learner.profile.role"
+          :model-value="Instructor.profile.role"
           :options="options.profileRoles"
         />
       </FormField>
-      <FormField label="State">
+      <FormField label="Assign Courses">
         <FormControl
-          :model-value="learner.profile.state"
+          placeholder="Search Course"
+        />
+      </FormField>
+      <template v-for="(value,item) in choose">
+        <h4 class="mt-5 mb-2 font-bold">
+          {{ item }}
+        </h4> 
+        <template v-for="i in optionstf">
+          <input type="radio" v-model="choose[item]" :value="i" />
+          {{ i }}
+        </template>
+        <br>
+      </template>
+
+
+      <FormField label="State" class="mt-6">
+        <FormControl
+          :model-value="Instructor.profile.state"
           :options="options.state"
         />
       </FormField>
       <FormField label="Language">
         <FormControl
-          :model-value="learner.profile.language"
+          :model-value="Instructor.profile.language"
           :options="options.language"
         />
       </FormField>
@@ -49,13 +66,13 @@
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <FormField label="User Segment">
         <FormControl
-          :model-value="learner.AdditionalDetails.UserSegment"
+          :model-value="Instructor.AdditionalDetails.UserSegment"
           :options="options.UserSegment"
         />
       </FormField>
       <FormField label="Lead Status">
         <FormControl
-          :model-value="learner.AdditionalDetails.LeadStatus"
+          :model-value="Instructor.AdditionalDetails.LeadStatus"
           :options="options.LeadStatus"
         />
       </FormField>
@@ -67,27 +84,6 @@
       <div class="flex flex-row-reverse">
         <BaseButton color="info" label="Save" />
       </div>
-    </div>
-
-    <h2 class="text-2xl mt-8 font-semibold">Unblock Learner on iOS</h2>
-    <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p>
-        Learner's accounts are blocked if they try to take more than 3 screenshots in the iOS app.
-On every screenshot taken, they are presented with the warning of account blockage.
-You can click on the below button to unblock the account
-      </p>
-      <br>
-      <h2>Total screenshots taken : <span class="text-blue-400">0</span></h2>
-      <div class="flex mt-4">
-        <BaseButton color="info" label="Unblock Learner" />
-      </div>
-    </div>
-
-
-
-    <h2 class="text-2xl mt-8 font-semibold">Downloaded Courses</h2>
-    <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      No Downloaded Courses
     </div>
 
     <h2 class="text-2xl mt-8 font-semibold">Change Password</h2>
@@ -111,20 +107,22 @@ You can click on the below button to unblock the account
 
     <h2 class="text-2xl mt-8 font-semibold">Referral Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p v-for="(value , key) in learner.ReferralDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
+      <p v-for="(value , key) in Instructor.ReferralDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
 
     </div>
 
     <h2 class="text-2xl mt-8 font-semibold">UTM Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
-      <p v-for="(value , key) in learner.UTMDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
+      <p v-for="(value , key) in Instructor.UTMDetails" class="py-2">{{key}}: <span class="font-bold">{{value}}</span></p>
 
     </div>
+
     <h2 class="text-2xl mt-8 font-semibold">Login Details</h2>
     <div class="p-6 bg-slate-100 dark:bg-transparent mt-5">
       <p class="py-2">Number of Logins : <span class="font-bold">60</span></p>
       <p class="py-2">Login Device Reset Count : <span class="font-bold">4</span></p>
       <p class="py-2">last login date : <span class="font-bold">{{new Date("Mar 03 , 2023")}}</span></p>
+
       <form class="relative" @submit.prevent="submit">
         <label for="msg-search" class="sr-only">Search</label>
         <input
@@ -183,6 +181,7 @@ You can click on the below button to unblock the account
 
     </div>
 
+
   </div>
 </template>
 
@@ -195,7 +194,7 @@ import BaseButtons from "@/components/Buttons/BaseButtons.vue";
 import BaseIcon from "@/components/Display/BaseIcon.vue";
 
 const props = defineProps({
-  learner: {
+  Instructor: {
     type: Object,
     required: true,
   },
@@ -204,10 +203,9 @@ const props = defineProps({
     required: true,
   },
 });
-
 const searchQuery = ref("");
 
-const items = ref(props?.learner?.LoginDetails?.['last logins'])
+const items = ref(props?.Instructor?.LoginDetails?.['last logins'])
 
 const filteredItems = computed(()=>{
   let filtered = items.value;
@@ -222,6 +220,22 @@ const filteredItems = computed(()=>{
   }
   return filtered
 })
+
+const optionstf = ["Yes" , "No"]
+const choose = ref({
+      "Can Edit Published Courses":"Yes",
+      "Need Approval for Publishing Courses":"Yes",
+      "Access of Live class Recordings":"No",
+      "Access of Sales Dashboard":"No",
+      "Access of Messenger":"No",
+      "Access of Bandwidth Reports":"Yes",
+      "Access of Usage Reports":"Yes",
+      "Access of Live tests Reports":"Yes",
+      "Access of Live class Reports":"No",
+      "Access to Learner Details":"No",
+      "Access to Download Quiz/Live Test":"Yes",
+      "Access to enroll learners in any course":"Yes",
+    })
 
 </script>
 

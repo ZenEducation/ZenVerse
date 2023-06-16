cc2b9890a8f539a5fc5e5cfa32aa80e22ae8df97
mohit soni <imohitsoni28@gmail.com>
2023-06-16 12:09:35 +0530

    quilEditor modeling values

--- components/LMS/QuilEditor.vue
+++ components/LMS/QuilEditor.vue
@@ -1,47 +1,84 @@
 <script setup>
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
-import BlotFormatter from 'quill-blot-formatter'
-import ImageUploader from 'quill-image-uploader';
-import axios from 'axios';
-import QuillBetterTable from 'quill-better-table'
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import BlotFormatter from "quill-blot-formatter";
+import ImageUploader from "quill-image-uploader";
+import { ref, computed } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  text:{
+    type: String,
+    default: "",
+  },
+});
+
+const quillEditorRef = ref(null);
+
 const toolbarOptions = [
-        [{'direction': 'rtl'}],
-        [{'size': ['small', false, 'large', 'huge']}],
-        [{'header':[1,2,3,4,5,6,false]}],
-        [{'font': []}],
-        [{'header':1},{'header':2}],
-        [{'color':[]},{'background':[]}],
-        ['bold','italic','underline','strike'],
-        [{'script': 'sub'}, {'script': 'super'}],
-        [{'list':'ordered'},{'list':'bullet'}],
-        [{'align':[]}],
-        [{'indent':'-1'},{'indent':'+1'}],
-        ['blockquote'],
-        ['image','video','link'],
-        ['code-block'],
-        ['clean']
-    ]
-const modules = { 
-        module: BlotFormatter, 
-        module: ImageUploader,
-      
-        options: {
-          upload: file => {
-          
-    
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve(
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"
-            );
-          }, 3500);
-        });
-      
-    
-          }
-        }}
+  [{ direction: "rtl" }],
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ header: 1 }, { header: 2 }],
+  [{ color: [] }, { background: [] }],
+  ["bold", "italic", "underline", "strike"],
+  [{ script: "sub" }, { script: "super" }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ align: [] }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  ["blockquote"],
+  ["image", "video", "link"],
+  ["code-block"],
+  ["clean"],
+];
+
+const modules = {
+  module: BlotFormatter,
+  module: ImageUploader,
+  options: {
+    upload: (file) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"
+          );
+        }, 3500);
+      });
+    },
+  },
+};
+const emit = defineEmits(["update:modelValue","update:text"]);
+
+const data = ref("");
+
+
+watchEffect(() => {
+  data.value = props.modelValue;
+});
+
+watchEffect(() => {
+  emit("update:modelValue", data.value);
+  emit("update:text" , quillEditorRef?.value?.getText());
+});
+
+console.log(props.modelValue, " : model");
+
+
 </script>
+
 <template>
-      <QuillEditor style="min-height: 30vh;" theme="snow" :toolbar="toolbarOptions"/>
+  <div>
+    <QuillEditor
+      style="min-height: 30vh"
+      theme="snow"
+      :toolbar="toolbarOptions"
+      content-type="html"
+      v-model:content="data"
+      ref="quillEditorRef"
+    />
+  </div>
 </template>
--- pages/LMS/addQuiz.vue
+++ pages/LMS/addQuiz.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { ref, reactive, onMounted } from "vue";
 import {
-    mdiBallot,
-    mdiAccount,
-    mdiCreditCardOutline,
-    mdiFileUploadOutline,
-    mdiDragVertical,
-    mdiTrashCanOutline,
-    mdiDotsVertical,
+  mdiBallot,
+  mdiAccount,
+  mdiCreditCardOutline,
+  mdiFileUploadOutline,
+  mdiDragVertical,
+  mdiTrashCanOutline,
+  mdiDotsVertical,
 } from "@mdi/js";
 
 import SectionMain from "@/components/Sections/SectionMain.vue";
@@ -22,163 +22,229 @@ import FormUploadFiles from "@/components/LMS/FormUploadFiles.vue";
 import Uploadtext from "@/components/LMS/Uploadtext.vue";
 import SeclectionMultipleButton from "@/components/Sections/SeclectionMultipleButton.vue";
 import QuilEditor from "@/components/LMS/QuilEditor.vue";
-import SectionTitleLineWithButton from "@/components/Sections/SectionTitleLineWithButton.vue"
-import SectionTitle from "@/components/Sections/SectionTitle.vue"
+import SectionTitleLineWithButton from "@/components/Sections/SectionTitleLineWithButton.vue";
+import SectionTitle from "@/components/Sections/SectionTitle.vue";
 import PremFormControlListbox from "~~/components/Forms/PremFormControlListbox.vue";
-
-
-
+import PremFormControl from "~~/components/Forms/PremFormControl.vue";
 const options = ref([
-    { id: 1, option: "option1" },
-    { id: 2, option: "option2" },
-
-])
+  { id: 1, option: "option1" },
+  { id: 2, option: "option2" },
+]);
+const QuestionTypeOptions = ["One Correct Answer", "Multiple Correct Answers"]
+const quiz = ref({
+  title: "question 1",
+  questions: [
+    {
+      title: "this is title of question",
+      titleText:"",
+      type: "One Correct Answer",
+      choices: [
+        { id: 1, title: "this is choice 1", isCorrect: true },
+        { id: 2, title: "this is choice 2", isCorrect: false },
+        { id: 3, title: "this is choice 3", isCorrect: false },
+      ],
+    },
+  ],
+});
 
 const totaloptions = ref([]);
 onMounted(() => {
-    totaloptions.value = options.value.map(item => ({
-        id: item.id,
-        name: `Transformed ${item.option}`
-    }));
+  totaloptions.value = options.value.map((item) => ({
+    id: item.id,
+    name: `Transformed ${item.option}`,
+  }));
 });
 
 const header = computed(() => {
-    if (inputValue.value) {
-        return `${inputValue.value}`;
-    } else {
-        return "Untitled Quiz";
-    }
+  if (inputValue.value) {
+    return `${inputValue.value}`;
+  } else {
+    return "Untitled Quiz";
+  }
 });
 const inputValue = ref("");
 const contentValue = ref("");
-const questionVal = ref("")
-const uploadQuestion = ref(false)
+const questionVal = ref("");
+const uploadQuestion = ref(false);
 
 const question = computed(() => {
-    if (questionVal.value) {
-        return `${questionVal.value}`;
-    } else {
-        return "What is your Question?";
-    }
-})
+  if (questionVal.value) {
+    return `${questionVal.value}`;
+  } else {
+    return "What is your Question?";
+  }
+});
 
 const upload = () => {
-    uploadQuestion.value = !uploadQuestion.value
-}
-
+  uploadQuestion.value = !uploadQuestion.value;
+};
 
 const addOption = () => {
-    const newOptionId = options.value.length + 1;
-    const newOption = {
-        id: newOptionId,
-        option: `option${newOptionId}`,
-    };
-    options.value.push(newOption);
-    totaloptions.value.push({
-        id: newOption.id,
-        name: `Transformed ${newOption.option}`,
-    });
+  const newOptionId = options.value.length + 1;
+  const newOption = {
+    id: newOptionId,
+    option: `option${newOptionId}`,
+  };
+  options.value.push(newOption);
+  totaloptions.value.push({
+    id: newOption.id,
+    name: `Transformed ${newOption.option}`,
+  });
 };
 const deleteOption = (index) => {
-    options.value.splice(index, 1);
-    totaloptions.value.splice(index, 1);
-    // Rearrange numbering
-    totaloptions.value.forEach((option, i) => {
-        option.id = i + 1;
-        option.name = `Transformed option${option.id}`;
-    });
+  options.value.splice(index, 1);
+  totaloptions.value.splice(index, 1);
+  // Rearrange numbering
+  totaloptions.value.forEach((option, i) => {
+    option.id = i + 1;
+    option.name = `Transformed option${option.id}`;
+  });
 };
 
 const questionType = ref("onecorrect");
 
-
-
 </script>
 
 <template>
-    <div>
-        <NuxtLayout name="zen">
-            <SectionMain>
-                <SeclectionMultipleButton :icon="mdiFileUploadOutline" :title="header" main>
-                    <BaseButton label="DISCARD CHANGES" :icon="mdiCreditCardOutline" rounded-full small />
-                    <BaseButton label="SAVE" :icon="mdiCreditCardOutline" color="contrast" rounded-full small />
-                </SeclectionMultipleButton>
+  <div>
+    <NuxtLayout name="zen">
+      <SectionMain>
+        <SeclectionMultipleButton
+          :icon="mdiFileUploadOutline"
+          :title="header"
+          main
+        >
+          <BaseButton
+            label="DISCARD CHANGES"
+            :icon="mdiCreditCardOutline"
+            rounded-full
+            small
+          />
+          <BaseButton
+            label="SAVE"
+            :icon="mdiCreditCardOutline"
+            color="contrast"
+            rounded-full
+            small
+          />
+        </SeclectionMultipleButton>
 
-                <div class="grid grid-cols-1 gap-6 mb-6">
-                    <CardBox :icon="mdiBallot" class="mb-6 lg:mb-0 xl:col-span-3" is-form @submit.prevent="submit">
-                        <PremFormField label="Title" horizontal>
-                            <FormUploadFiles v-model="inputValue" :icon-left="mdiAccount" help="Title" />
-                        </PremFormField>
-
-                    </CardBox>
-                    <div class="flex flex-col">
-                        <CardBox>
-                            <div class=" flex">
-                                <SectionTitleLineWithButton :title="'Question#1 ' + question">
-
-                                </SectionTitleLineWithButton>
-
-                            </div>
-                            <PremFormField label="Question Type" horizontal>
-                                <select name="questiontype" v-model="questionType" id="questiontype">
-                                    <option value="onecorrect">One Correct Answer</option>
-                                    <option value="multiplecorrect">Multiple Correct Answers</option>
-                                </select>
-                            </PremFormField>
-
-                            <PremFormField label="Question" horizontal>
-                                <QuilEditor v-model="questionVal"></QuilEditor>
-                            </PremFormField>
+        <div class="grid grid-cols-1 gap-6 mb-6">
+          <CardBox
+            :icon="mdiBallot"
+            class="mb-6 lg:mb-0 xl:col-span-3"
+            is-form
+            @submit.prevent="submit"
+          >
+            <PremFormField label="Title" horizontal>
+              <FormUploadFiles
+                v-model="inputValue"
+                :icon-left="mdiAccount"
+                help="Title"
+              />
+            </PremFormField>
+          </CardBox>
 
 
-                            <div v-if="questionType == 'onecorrect'" class="my-2"
-                                v-for="(totaloption, index) in totaloptions" :key="totaloption.id">
-                                <PremFormField :label="'Choice ' + totaloption.id">
-                                    <QuilEditor />
+          <div class="flex flex-col" v-for="(que ,index ) in quiz.questions"   >
+            <CardBox>
+              <div class="flex">
+                <SectionTitleLineWithButton :title="'Question#'+(index+1) + que.titleText">
+                </SectionTitleLineWithButton>
+                                <SectionTitleLineWithButton :title="'Question#'+(index+1) + que.title">
+                </SectionTitleLineWithButton>
+              </div>
+              <PremFormField label="Question Type" horizontal>
+                <PremFormControl :options="QuestionTypeOptions" :modelValue="que.type" />
+              </PremFormField>
 
-                                </PremFormField>
-                                <input type="radio" name="option" id="'option' + totaloption.id">
-                                <label for="'option' + totaloption.id" class="ml-2">This is the Correct Answer</label>
-                                <br>
-                                <button @click="deleteOption(index)">Delete</button>
-                            </div>
-                            <div v-else class="my-2" v-for="(totaloption, index) in totaloptions" :key="index">
-                                <PremFormField :label="'Choice ' + totaloption.id">
-                                    <QuilEditor />
+              <PremFormField label="Question" horizontal>
+                <QuilEditor v-model="que.title" v-model:text="que.titleText"></QuilEditor>
+              </PremFormField>
 
-                                </PremFormField>
-                                <input type="checkbox" class="rounded-3xl" name="'option' + totaloption.id"
-                                    id="'option' + totaloption.id">
-                                <label for="'option' + totaloption.id" class="ml-2">This is the Correct Answer</label>
-                                <br>
-                                <button @click="deleteOption(index)">Delete</button>
-                            </div>
-                            <button @click="addOption" class=" bg-blue-700 p-3 rounded-md text-white">Add Choice</button>
-                            <PremFormField label="Explanation" horizontal>
-                                <QuilEditor />
-                            </PremFormField>
-                        </CardBox>
-                        <div class="flex my-5 gap-3">
-                            <button class=" bg-blue-700 p-3 rounded-md text-white">Add Question</button>
-                            <button @click="upload"
-                                class=" bg-white border border-blue-700 p-3 rounded-md text-blue-700">Import More
-                                Questions</button>
-                        </div>
-                        <CardBox v-show="uploadQuestion">
-                            <PremFormField label="" horizontal >
-                                <Uploadtext pdffile downloadlist :icon="mdiDragVertical" :iconRight="mdiTrashCanOutline"
-                                    dragText="Drag & Drop an XLSX file to import"  :icon-left="mdiAccount"
-                                    help="Upload a video file" placeholder="Upload a video file" />
-                            </PremFormField>
-                        </CardBox>
-                    </div>
+              <div
+                v-if="questionType == 'onecorrect'"
+                class="my-2"
+                v-for="(totaloption, index) in totaloptions"
+                :key="totaloption.id"
+              >
+                <PremFormField :label="'Choice ' + totaloption.id">
+                  <QuilEditor />
+                </PremFormField>
+                <input
+                  type="radio"
+                  name="option"
+                  id="'option' + totaloption.id"
+                />
+                <label for="'option' + totaloption.id" class="ml-2"
+                  >This is the Correct Answer</label
+                >
+                <br />
+                <button @click="deleteOption(index)">Delete</button>
+              </div>
+              <div
+                v-else
+                class="my-2"
+                v-for="(totaloption, index) in totaloptions"
+                :key="index"
+              >
+                <PremFormField :label="'Choice ' + totaloption.id">
+                  <QuilEditor />
+                </PremFormField>
+                <input
+                  type="checkbox"
+                  class="rounded-3xl"
+                  name="'option' + totaloption.id"
+                  id="'option' + totaloption.id"
+                />
+                <label for="'option' + totaloption.id" class="ml-2"
+                  >This is the Correct Answer</label
+                >
+                <br />
+                <button @click="deleteOption(index)">Delete</button>
+              </div>
+              <button
+                @click="addOption"
+                class="bg-blue-700 p-3 rounded-md text-white"
+              >
+                Add Choice
+              </button>
+              <PremFormField label="Explanation" horizontal>
+                <QuilEditor />
+              </PremFormField>
+            </CardBox>
+            <div class="flex my-5 gap-3">
+              <button class="bg-blue-700 p-3 rounded-md text-white">
+                Add Question
+              </button>
+              <button
+                @click="upload"
+                class="bg-white border border-blue-700 p-3 rounded-md text-blue-700"
+              >
+                Import More Questions
+              </button>
+            </div>
+            <CardBox v-show="uploadQuestion">
+              <PremFormField label="" horizontal>
+                <Uploadtext
+                  pdffile
+                  downloadlist
+                  :icon="mdiDragVertical"
+                  :iconRight="mdiTrashCanOutline"
+                  dragText="Drag & Drop an XLSX file to import"
+                  :icon-left="mdiAccount"
+                  help="Upload a video file"
+                  placeholder="Upload a video file"
+                />
+              </PremFormField>
+            </CardBox>
+          </div>
+        </div>
+      </SectionMain>
+    </NuxtLayout>
+  </div>
 
-                </div>
-            </SectionMain>
-        </NuxtLayout>
-    </div>
+
+
+
 </template>
-
-
-  
-

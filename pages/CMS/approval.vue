0e6dcad8a3f63276549a53efa1f165f056972301
shubhomrawat <shubhomrawat27@gmail.com>
2023-07-01 16:36:52 +0530

    approvalpage

--- pages/CMS/approval.vue
+++ pages/CMS/approval.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div :class="{ dark: styleStore.darkMode, 'overflow-hidden lg:overflow-visible': layoutStore.isAsideMobileExpanded }">
-      <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': layoutStore.isAsideMobileExpanded }]"
-        class="pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
+<div :class="{ dark: styleStore.darkMode, 'overflow-hidden lg:overflow-visible': layoutStore.isAsideMobileExpanded }">
+  <div :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': layoutStore.isAsideMobileExpanded }]"
+    class="pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
         <!-- The Navbar -->
         <NavBar :menu="menuNavBar" :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': layoutStore.isAsideMobileExpanded }]"
           @menu-click="menuClick">
@@ -13,30 +13,46 @@
             <BaseIcon :path="layoutStore.isAsideLgActive ? mdiBackburger : mdiMenu" size="24" />
           </NavBarItemPlain>
           <NavBarItemPlain use-margin>
-            <FormControl placeholder="Search (ctrl+k)" ctrl-k-focus transparent borderless v-model="searchQuery" />
+            <FormControl placeholder="Search (ctrl+k)" ctrl-k-focus transparent borderless v-model="searchQuery"
+              class="text-m w-60 h-8" />
           </NavBarItemPlain>
         </NavBar>
 
         <PremAsideMenu :menu="menuAside" @menu-click="menuClick" />
-
-<div class="approval-page">
-  <div class="header">
-    <div class="header-content">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNACCdEsX-Kr8ph3knqv14gsaeGYop-uiO4g&usqp=CAU" alt="Header Image" style="width: 70px; height: auto;">
-      <h1 :class="{'dark-mode-title': styleStore.darkMode}">{{ title }}</h1>
+ <div class="mobile-view">
+     
     </div>
-    <hr style="height: 4.5px; border: none; margin: 0; background-color: grey;">
-  </div>
-</div>
+        <div class="approval-page">
+          <div class="header">
+            <div class="header-content">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNACCdEsX-Kr8ph3knqv14gsaeGYop-uiO4g&usqp=CAU" alt="Header Image" style="width: 70px; height: auto;">
+              <h1 :class="{'dark-mode-title': styleStore.darkMode}">{{ title }}</h1>
+            </div>
+            <hr style="height: 4.5px; border: none; margin: 0; background-color: grey;">
+          </div>
+        </div>
 
-    <div class="search-filter">
+        <div class="search-filter">
           <input
             type="text"
             v-model="searchQuery"
-            placeholder="Search by Approvee name, Title, or ID"
+            placeholder=" Search by Approvee name, Title, ID, Type, or Category "
           />
-          <button @click="resetFilters">Reset</button>
+          <div>
+            <label for="filterBy">Filter by Approvee:</label>
+            <select id="filterBy" v-model="filterBy">
+              <option value="id">ID</option>
+              <option value="title">Title</option>
+              <option value="type">Type</option>
+              <option value="category">Category</option>
+              <option value="approvee">Approvee</option>
+              <option value="status">Status</option>
+              <option value="submittedOn">Submitted On</option>
+            </select>
+          </div>
+          <button class="reset-button" @click="resetFilters">Reset</button>
         </div>
+  
 
      <div class="table-container">
           <table class="data-table">
@@ -57,19 +73,19 @@
               <tr v-for="row in filteredRows" :key="row.id">
                 <td>{{ row.id }}</td>
                <td>{{ row.title }}</td>
-               <div class="submission-cell">
-                        <a :href="row.submission" target="_blank">{{ row.submission }}</a>
-                   <img v-if="row.submissionImage" :src="row.submissionImage" style="display: block; margin: 0 35px; width: 130px; height: auto;" />
-                    </div>
+               <td>
+              <a :href="row.submission" target="_blank">{{ row.submission }}</a>
+              <img v-if="row.submissionImage" :src="row.submissionImage" style="display: block; margin: 0 35px; width: 130px; height: auto;" />
+                </td>
                 <td>{{ row.type }}</td>
                <td>{{ row.category }}</td>
                <td> <div class="approvee-avatar">
-               <img :src="row.avatar" alt="Approvee Avatar" />
-                <span>{{ row.approvee }}</span>
-                </div></td>
-               <td>{{ row.status }}</td>
-               <td>{{ row.submittedOn }}</td>
-                <td>
+              <img :src="row.avatar" alt="Approvee Avatar" />
+            <span>{{ row.approvee }}</span>
+              </div></td>
+                <td>{{ row.status }}</td>
+                 <td>{{ row.submittedOn }}</td>
+                  <td>
                   <button class="small-button transparent-button" @click="view(row)">
                        <span class="mdi mdi-eye"></span> View</button>
                        <button class="small-button transparent-button with-border" @click="approve(row)"> Approve</button>
@@ -172,25 +188,22 @@ const rows = [
 
 const searchQuery = ref("");
 
+const filterBy = ref("id");
+
 const filteredRows = computed(() => {
   const query = searchQuery.value.toLowerCase().trim();
   if (!query) {
     return rows;
   } else {
-    return rows.filter(
-      (row) =>
-        row.approvee.toLowerCase().includes(query) ||
-        row.title.toLowerCase().includes(query) ||
-        row.id.toString().includes(query) ||
-        row.type.toLowerCase().includes(query) ||
-        row.status.toLowerCase().includes(query) ||
-        row.category.toLowerCase().includes(query)
+    return rows.filter((row) =>
+      row[filterBy.value].toLowerCase().includes(query)
     );
   }
 });
 
 const resetFilters = () => {
-  searchQuery.value = ""; 
+  searchQuery.value = "";
+  filterBy.value = "id";
 };
 
 useMainStore().setUser({
@@ -251,19 +264,21 @@ const title = computed(() => {
 </script>
 
 <style>
+
 .header-content {
   display: flex;
   align-items: center;
 }
+
 .header-content {
   margin-bottom: 10px;
-  color: #000; 
+  color: #000;
   margin-left: 20px;
 }
 
 .approval-page {
   padding: 10px;
-  color:black;
+  color: black;
 }
 
 .darkMode .header-title {
@@ -272,65 +287,121 @@ const title = computed(() => {
 
 .header {
   padding: 10px;
-  color:black;
+  color: black;
   font: 1em sans-serif;
   font-size: 35px;
-   text-align: left;
+  text-align: left;
 }
+
 .search-filter {
   display: flex;
   align-items: center;
-  gap: 860px;
-  padding-left: 20px;
+  justify-content: space-between;
+  gap: 300px;
+  padding: 10px 20px;
   margin-bottom: 20px;
+  margin-left: 20px; /* Add left margin */
+  margin-right: 20px; /* Add right margin */
+}
+
+.search-filter input[type='text'] {
+  flex: 1;
+  padding: 12px;
+  border: 1px solid grey;
+  border-radius: 40px;
+  font-size: 16px;
+}
+
+.search-filter label {
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.search-filter select {
+  padding: 8px;
+  border: 1px solid grey;
+  border-radius: 4px;
+  font-size: 14px;
+}
+
+.search-filter button {
+  padding: 6px 10px;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.search-filter button:hover {
+  background-color: #4b84c1;
+}
+
+@media (max-width: 768px) {
+  .search-filter {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+  }
+
+  .search-filter input[type='text'] {
+    width: 100%;
+  }
+}
+
+.reset-button {
+  padding: 6px 10px;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+}
+
+@media (max-width: 768px) {
+  .search-filter {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+  }
+
+  .reset-button {
+    width: 100%;
+  }
 }
 
 .table-container {
   width: 100%;
-  border-collapse: collapse;
+  margin-left: 15px; /* Add left margin */
+  margin-right: 15px; /* Add right margin */
+  overflow-x: auto; /* Enable horizontal scrolling on small screens */
 }
 
 .table-container th,
-.approval-table td {
+.table-container td {
   padding: 10px;
-  border-bottom: 2px solid lightblue;
 }
 
 .data-table th {
   text-align: left;
 }
 
-.approval-table td:last-child {
+.table-container td:last-child {
   text-align: center;
 }
 
-button {
-  padding: 6px 10px;
-  background-color: #007bff;
-  border: none;
-    border-radius: 5px;
-  color: #fff;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #4b84c1;
+.approval-table td:last-child {
+  text-align: center;
 }
 
 .button-container {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 4px;
 }
 
 .small-button {
   font-size: 14px;
   padding: 9px 12px;
-}
-
-.small-button {
-  font-size: 14px;
-  padding: 6px 10px;
 }
 
 .transparent-button {
@@ -342,11 +413,12 @@ button:hover {
 .with-border {
   border: 1px solid #000;
 }
+
 input[type='text'] {
   padding: 12px;
   border: 1px solid grey;
   border-radius: 40px;
-  width: 300px;
+  width: 100%; /* Adjust the width value to your preference */
   font-size: 16px;
 }
 
@@ -358,6 +430,7 @@ input[type='text'] {
   display: flex;
   align-items: center;
   gap: 8px;
+  justify-content: center; /* Center the approvee images */
 }
 
 .approvee-avatar img {
@@ -365,6 +438,7 @@ input[type='text'] {
   height: 24px;
   border-radius: 50%;
 }
+
 .pagination {
   display: flex;
   margin-right: 30px;
@@ -376,6 +450,13 @@ input[type='text'] {
   font-size: 14px;
   color: black;
 }
+
+.submission-text {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
 </style>
 
 

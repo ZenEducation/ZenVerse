da8f97dfb6e814c3cc2fe5522c6eb1f2a0fecaf7
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-07-16 20:50:01 +0530

    Conflic Resolve

--- components/CMS/Editor.vue
+++ components/CMS/Editor.vue
@@ -1,10 +1,7 @@
 <script setup>
 import BaseButton from "~~/components/Buttons/BaseButton.vue";
 import PremFormField from "@/components/Forms/FormField.vue";
-import PremFormControl from "@/components/Forms/FormControl.vue";
-import { Marked } from '@ts-stack/markdown';
-import TurndownService from 'turndown';
-import { ref, onMounted, } from 'vue';
+import { onMounted, } from 'vue';
 
 import {
     mdiFormatBold, mdiFormatItalic, mdiStickerEmoji, mdiPound, mdiAt
@@ -15,30 +12,6 @@ onMounted(() => {
     document.execCommand('defaultParagraphSeparator', false, 'p');
 });
 
-function convertToMarkdown(inputValue) {
-    const turndown = new TurndownService({
-        emDelimiter: '_',
-        linkStyle: 'inlined',
-        headingStyle: 'atx',
-    });
-
-    try {
-        if (inputValue instanceof Element) {
-            console.error('InputValue is an Element. Skipping the conversion.');
-            return;
-        }
-
-        const markdown = turndown.turndown(inputValue);
-        innerValue.value = inputValue;
-        emit('update:value', markdown.trim());
-    } catch (error) {
-        console.error('Error converting content to Markdown:', error);
-        innerValue.value = inputValue;
-        emit('update:value', inputValue.trim());
-    }
-}
-
-
 function applyBold() {
     document.execCommand('bold');
 }
@@ -47,9 +20,6 @@ function applyBold() {
 function applyItalic() {
     document.execCommand('italic');
 }
-
-
-
 
 function insertCharacter(character) {
     const editor = document.querySelector('.text-output');
@@ -66,76 +36,43 @@ function insertCharacter(character) {
 
     editor.focus();
 }
-
-// The emit function is automatically available in the script setup.
-// Also, `emit("input", ...)` should be replaced with `emit("update:value", ...)`
-
-
-
-
-
 </script>
 <template>
     <div>
 
         <div @input="onInput" v-html="innerValue" :contenteditable="true"
             class="text-output p-4 border-none bg-blue-50 dark:bg-slate-800" placeholder />
-
-
-
-
-
-
-
-
-
         <PremFormField class="mt-2">
             <div class="flex place-content-end flex-wrap">
-                <BaseButton @click="applyBold" class="button dark:bg-slate-900" color="" :icon="mdiFormatBold"
-                    iconSize="30px" />
-                <BaseButton @click="applyItalic" class="button dark:bg-slate-900" color="" :icon="mdiFormatItalic"
-                    iconSize="30px" />
-                <BaseButton @click="applyEmoji" class="button dark:bg-slate-900" color="" :icon="mdiStickerEmoji"
-                    iconSize="30px" />
-                <BaseButton @click="insertCharacter('#')" class="button dark:bg-slate-900" color="" :icon="mdiPound"
-                    iconSize="30px" />
-                <BaseButton @click="insertCharacter('@')" class="button dark:bg-slate-900" color="" :icon="mdiAt"
-                    iconSize="30px" />
+                <BaseButton @click="applyBold"
+                    class="border-2 border-gray-300 rounded-lg px-3 py-1 mb-3 mr-3 dark:bg-slate-900 hover:border-green-300;"
+                    color="" :icon="mdiFormatBold" iconSize="30px" />
+                <BaseButton @click="applyItalic"
+                    class="border-2 border-gray-300 rounded-lg px-3 py-1 mb-3 mr-3 dark:bg-slate-900 hover:border-green-300;"
+                    color="" :icon="mdiFormatItalic" iconSize="30px" />
+                <BaseButton @click="applyEmoji"
+                    class="border-2 border-gray-300 rounded-lg px-3 py-1 mb-3 mr-3 dark:bg-slate-900 hover:border-green-300;"
+                    color="" :icon="mdiStickerEmoji" iconSize="30px" />
+                <BaseButton @click="insertCharacter('#')"
+                    class="border-2 border-gray-300 rounded-lg px-3 py-1 mb-3 mr-3 dark:bg-slate-900 hover:border-green-300;"
+                    color="" :icon="mdiPound" iconSize="30px" />
+                <BaseButton @click="insertCharacter('@')"
+                    class="border-2 border-gray-300 rounded-lg px-3 py-1 mb-3 mr-3 dark:bg-slate-900 hover:border-green-300;"
+                    color="" :icon="mdiAt" iconSize="30px" />
             </div>
         </PremFormField>
 
     </div>
 </template>
-  
-
-
-
-  
-
-  
 
 <style scoped>
-.button {
-    @apply border-2;
-    @apply border-gray-300;
-    @apply rounded-lg;
-    @apply px-3 py-1;
-    @apply mb-3 mr-3;
-}
-
-.button:hover {
-    @apply border-green-300;
-}
-
 .text-output {
     height: 150px;
     width: 100%;
 }
 
 .text-output h1 {
-    @apply text-2xl;
-    @apply font-bold;
-    @apply pb-4;
+    @apply text-2xl font-bold pb-4;
 }
 
 .text-output p {

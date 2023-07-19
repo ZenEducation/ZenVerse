a12c67dfaab569986cda3fd7b341ce2fd69c3ee1
chaitanya <chaitanya.tata215@gmail.com>
2023-07-20 01:00:36 +0530

    Added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "HelpKnowledgeAndDocsIntegration": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}

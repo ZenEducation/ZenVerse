8d1f49f184ac365ddda7c922bf2e9d2c04c4d0db
Pratish Ninawe <pratish.1006@gmail.com>
2023-07-23 11:00:13 +0530

    new form fields in jobapply and changes in menuaside

--- pages/RecruitmentPortal/Candidate/JobApply.vue
+++ pages/RecruitmentPortal/Candidate/JobApply.vue
@@ -2,21 +2,17 @@
   <NuxtLayout name="zen">
     <div class="font-nunito text-base">
       <!-- Start Hero -->
-      <section
-      class="relative table w-full py-36 lg:py-44 bg-black opacity-80 bg-no-repeat bg-center bg-cover"
-    >
-      <div >
-        <div class="grid grid-cols-1 pb-8 text-center mt-10">
-          <h3
-            class="mb-4 md:text-4xl text-xl md:leading-normal leading-normal font-medium text-white"
-          >
-            Web Developer
-          </h3>
+      <section class="relative table w-full py-36 lg:py-44 bg-black opacity-80 bg-no-repeat bg-center bg-cover">
+        <div>
+          <div class="grid grid-cols-1 pb-8 text-center mt-10">
+            <h3 class="mb-4 md:text-4xl text-xl md:leading-normal leading-normal font-medium text-white">
+              Web Developer
+            </h3>
+          </div>
+          <!--end grid-->
         </div>
-        <!--end grid-->
-      </div>
-      <!--end container-->
-    </section>
+        <!--end container-->
+      </section>
       <!--end section-->
 
       <!-- End Hero -->
@@ -26,112 +22,134 @@
         <div class="container">
           <div class="grid lg:grid-cols-12 grid-cols-1" id="reserve-form">
             <div class="lg:col-start-2 lg:col-span-10">
-              <div
-                class="rounded-md shadow dark:shadow-gray-800 bg-white dark:bg-slate-900 p-6"
-              >
+              <div class="rounded-md shadow dark:shadow-gray-800 bg-white dark:bg-slate-900 p-6">
                 <form>
                   <div class="grid lg:grid-cols-12 gap-6">
                     <div class="lg:col-span-6">
                       <div class="ltr:text-left rtl:text-right">
-                        <FormField
-                          label="Name"
-                          help="Required. Your name"
-                          :error="errorMsgs.name"
-                        >
-                          <FormControl
-                            v-model="formData.name"
-                            name="username"
-                            required
-                            autocomplete="username"
-                          />
+                        <FormField label="Name" help="Required. Your name" :error="errorMsgs.name">
+                          <FormControl v-model="formData.name" name="username" required autocomplete="username" />
                         </FormField>
                       </div>
                     </div>
 
                     <div class="lg:col-span-6">
                       <div class="ltr:text-left rtl:text-right">
-                        <FormField
-                          label="E-mail"
-                          help="Required. Your e-mail"
-                          :error="errorMsgs.email"
-                        >
-                          <FormControl
-                            v-model="formData.email"
-                            type="email"
-                            name="email"
-                            required
-                            autocomplete="email"
-                          />
+                        <FormField label="E-mail" help="Required. Your e-mail" :error="errorMsgs.email">
+                          <FormControl v-model="formData.email" type="email" name="email" required autocomplete="email" />
                         </FormField>
                       </div>
                     </div>
 
                     <div class="lg:col-span-6">
                       <div class="ltr:text-left rtl:text-right">
-                        <FormField
-                          label="Phone No"
-                          help="Required. Phone No.:"
-                          :error="errorMsgs.phone"
-                        >
-                          <FormControl
-                            v-model="formData.phone"
-                            name="phone"
-                            required
-                            autocomplete="phone"
-                          />
+                        <FormField label="Phone No" help="Required. Phone No.:" :error="errorMsgs.phone">
+                          <FormControl v-model="formData.phone" name="phone" required autocomplete="phone" />
                         </FormField>
                       </div>
                     </div>
 
                     <div class="lg:col-span-6">
                       <div class="ltr:text-left rtl:text-right">
-                        <FormField
-                          label="Job Title:"
-                          help="Required. Your Job Title:"
-                          :error="errorMsgs.jobTitle"
-                        >
-                          <FormControl
-                            v-model="formData.jobTitle"
-                            type="email"
-                            name="email"
-                            required
-                            autocomplete="email"
-                          />
+                        <FormField label="Job Title:" help="Required. Your Job Title:" :error="errorMsgs.jobTitle">
+                          <FormControl v-model="formData.jobTitle" type="email" name="email" required
+                            autocomplete="email" />
                         </FormField>
                       </div>
                     </div>
 
                     <div class="lg:col-span-6">
-                      <FormField
-                        label="Types of Job:"
-                        help="Required. Your Job Type:"
-                        :error="errorMsgs.jobType"
-                      >
-                        <FormControl
-                          v-model="formData.jobType"
-                          type="select"
-                          name="email"
-                          required
-                          autocomplete="email"
-                          :options="jobTypes"
-                        />
+                      <FormField label="Types of Job:" help="Required. Your Job Type:" :error="errorMsgs.jobType">
+                        <FormControl v-model="formData.jobType" type="select" name="email" required autocomplete="email"
+                          :options="jobTypes" />
                       </FormField>
                     </div>
 
+
+                    <!-- New Fields start -->
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="Skills:" help="Required. Your skills" :error="errorMsgs.skills">
+                          <FormControl v-model="formData.skills" type="text" name="skills" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="Date Of Birth:" help="Required. Your date of birth"
+                          :error="errorMsgs.dateOfBirth">
+                          <FormControl v-model="formData.dateOfBirth" type="date" name="dateOfBirth" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="Address:" help="Required. Address:" :error="errorMsgs.address">
+                          <FormControl v-model="formData.address" type="text" name="address" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="City:" help="Required. Your city" :error="errorMsgs.city">
+                          <FormControl v-model="formData.city" type="text" name="city" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="Country:" help="Required. Your country" :error="errorMsgs.country">
+                          <FormControl v-model="formData.country" type="text" name="country" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="Postal Code:" help="Required. Your postal code" :error="errorMsgs.postalCode">
+                          <FormControl v-model="formData.postalCode" type="number" name="postalCode" required />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="LinkedIn:" help="Your LinkedIn profile" :error="errorMsgs.linkedIn">
+                          <FormControl v-model="formData.linkedIn" type="text" name="linkedIn" />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <div class="ltr:text-left rtl:text-right">
+                        <FormField label="GitHub:" help="Your GitHub profile" :error="errorMsgs.gitHub">
+                          <FormControl v-model="formData.gitHub" type="text" name="gitHub" />
+                        </FormField>
+                      </div>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <FormField label="Experience:" help="Your experience" :error="errorMsgs.experience">
+                        <FormControl v-model="formData.experience" type="textarea" name="experience" required />
+                      </FormField>
+                    </div>
+
+                    <div class="lg:col-span-6">
+                      <FormField label="Projects links:" help="Your project links" :error="errorMsgs.projects">
+                        <FormControl v-model="formData.projects" type="text" name="projects" required />
+                      </FormField>
+                    </div>
+                    <!-- New Fields end -->
+
                     <div class="lg:col-span-12">
                       <div class="ltr:text-left rtl:text-right">
-                        <FormField
-                          label="Your Comment"
-                          help="Required. Your comment"
-                          :error="errorMsgs.comment"
-                        >
-                          <FormControl
-                            v-model="formData.comment"
-                            type="textarea"
-                            name="email"
-                            required
-                            autocomplete="email"
-                          />
+                        <FormField label="Your Comment" help="Required. Your comment" :error="errorMsgs.comment">
+                          <FormControl v-model="formData.comment" type="textarea" name="email" required
+                            autocomplete="email" />
                         </FormField>
                       </div>
                     </div>
@@ -142,13 +160,8 @@
                       </FormField>
                     </div>
                   </div>
-                  <button
-                    type="submit"
-                    @click.prevent="submitHandler"
-                    id="submit"
-                    name="send"
-                    class="btn p-3 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md mt-4"
-                  >
+                  <button type="submit" @click.prevent="submitHandler" id="submit" name="send"
+                    class="btn p-3 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md mt-4">
                     Send Message
                   </button>
                 </form>
@@ -181,6 +194,17 @@ const formData = ref({
   jobType: "",
   comment: "",
   uploadFile: "",
+  // new fields
+  skills: "",
+  dateOfBirth: "",
+  city: "",
+  country: "",
+  postalCode: "",
+  linkedIn: "",
+  gitHub: "",
+  experience: "",
+  projects: "",
+  address: "",
 });
 
 const jobTypes = ref([
@@ -199,6 +223,17 @@ const errorMsgs = ref({
   jobType: "",
   comment: "",
   uploadFile: "",
+  // new fields
+  skills: "",
+  dateOfBirth: "",
+  city: "",
+  country: "",
+  postalCode: "",
+  linkedIn: "",
+  gitHub: "",
+  experience: "",
+  projects: "",
+  address: "",
 });
 
 const submitHandler = () => {
@@ -207,6 +242,14 @@ const submitHandler = () => {
   validateRequired("jobType", formData.value.jobType);
   validateEmail(formData.value.email);
   validatePhone(formData.value.phone);
+  // validate new fields
+  validateRequired("dateOfBirth", formData.value.dateOfBirth);
+  validateRequired("city", formData.value.city);
+  validateRequired("country", formData.value.country);
+  validateRequired("postalCode", formData.value.postalCode);
+  validateRequired("experience", formData.value.experience);
+  validateRequired("projects", formData.value.projects);
+  validateRequired("address", formData.value.address);
 };
 
 const validateRequired = (field, val) => {

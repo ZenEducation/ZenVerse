7b650e1ba984346c93862d03a0e8371a9496b4ea
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2023-08-26 17:00:15 +0530

    CSS and Responsivenes Fixed

--- app.vue
+++ app.vue
@@ -4,19 +4,19 @@ import { darkModeKey, styleKey } from "@/configs/config";
 import { useMainStore } from "@/stores/main.js";
 import "@/assets/css/main.css";
 import '@aws-amplify/ui-vue';
-import { Amplify, Auth } from "aws-amplify";
-import awsconfig from '@/src/aws-exports'; 
+// import { Amplify, Auth } from "aws-amplify";
+// import awsconfig from '@/src/aws-exports'; 
 const url = window.location.host
-if (url.includes('localhost')) {
-  awsconfig.oauth.redirectSignIn = 'http://localhost:3000/'
-  awsconfig.oauth.redirectSignOut = 'http://localhost:3000/'
-}else{
-  awsconfig.oauth.redirectSignIn = 'https://dev.d1zumicxzxxdjk.amplifyapp.com/'
-  awsconfig.oauth.redirectSignOut = 'https://dev.d1zumicxzxxdjk.amplifyapp.com/'
-}
-console.log(awsconfig)
-Amplify.configure(awsconfig);
-Auth.configure(awsconfig)
+// if (url.includes('localhost')) {
+//   awsconfig.oauth.redirectSignIn = 'http://localhost:3000/'
+//   awsconfig.oauth.redirectSignOut = 'http://localhost:3000/'
+// }else{
+//   awsconfig.oauth.redirectSignIn = 'https://dev.d1zumicxzxxdjk.amplifyapp.com/'
+//   awsconfig.oauth.redirectSignOut = 'https://dev.d1zumicxzxxdjk.amplifyapp.com/'
+// }
+// console.log(awsconfig)
+// Amplify.configure(awsconfig);
+// Auth.configure(awsconfig)
 
 const mainStore = useMainStore();
 const styleStore = useStyleStore();
